101f98f8281e65ba9ef9e96603594a08b0eacf35
mikle <zimboroda@gmail.com>
2022-07-25 13:17:51 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'max-len': [2, { code: 120 }],
+    semi: ['error', 'never'],
+    'class-methods-use-this': 'off',
+    'no-unexpected-multiline': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    '@typescript-eslint/semi': [2, 'never'],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      {
+        allowExpressions: true,
+        allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
+  },
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+const IndexPage = () => {
+  return (
+    <div className="container m-auto">
+      <h1 className="text-3xl font-bold text-red-500 underline sm:scale-0 md:text-clip xl:saturate-100">
+        Hello world!
+      </h1>
+      <h1 className="underline sm:scale-0 sm:text-red-500 md:text-clip md:text-3xl xl:font-bold xl:saturate-100">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default IndexPage
